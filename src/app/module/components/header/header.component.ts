13336cd6a5ec51a73c1392091dcf30ab39f28e80
Alejo12680 <alejo152725@gmail.com>
2024-03-29 03:10:12 -0500

    button de cambiar tema color

--- src/app/module/components/header/header.component.ts
+++ src/app/module/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent  implements OnInit {
 
+  // Resive una propiedades, para poder Reutilizar el componente, con el metodo Input, el ? sirve para iniciarla en vacio y evitar el error, tambien se puede quitar en tsconfig.json con el "strict": pasarlo a (false) o colocar el '| undefined'.
+  @Input() title: string | undefined;
+  @Input() backButton: string | undefined;
+  @Input() isModal: boolean | undefined;
+  @Input() color: string | undefined;
+  @Input() centerTitle: boolean | undefined;
+  
+  public darkModel: boolean = false;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+
+  }
+
+  // Para cambiar el color realmente hay que irse a 'theme/variables.css/ y borramos la propiedad @media (prefers-color-scheme: dark) para pegar en los bodys esto [color-theme="darck"]
+  setColor(darkModel: boolean) {
+    this.darkModel = darkModel;
+  }
 
 }
